a09f1d1ece341e9a9d075e45c6ace4d9bd284c34
Nathan Evans <nathan@de1ux.com>
2023-04-10 21:07:54 -0600

    16: fix mixin

--- api/models/base.py
+++ api/models/base.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 
-class BaseModel(object):
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
--- api/models/sensor.py
+++ api/models/sensor.py
@@ -3,12 +3,12 @@ from django.db import models
 from api.models.base import BaseModel
 
 
-class Sensor(models.Model, BaseModel):
+class Sensor(BaseModel):
     id = models.TextField(primary_key=True)
     last_heartbeat = models.DateTimeField(auto_now_add=True)
 
 
-class SensorCommand(models.Model, BaseModel):
+class SensorCommand(BaseModel):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     command = models.TextField()
     acked = models.BooleanField()
