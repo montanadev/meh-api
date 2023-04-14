b58dc5e691ca19f1c4427e9e139b7016cfa54183
Nathan Evans <nathan@de1ux.com>
2023-04-13 21:07:48 -0600

    Publish leds on membership

---
+++ api/management/commands/subscribe.py
@@ -0,0 +1,25 @@
+import logging
+import time
+
+import paho.mqtt.client as mqtt
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # The callback function of connection
+        def on_connect(client, userdata, flags, rc):
+            print(f"Connected with result code {rc}")
+            client.subscribe("test_sensor")
+
+        # The callback function for received message
+        def on_message(client, userdata, msg):
+            print(msg.topic + " " + str(msg.payload))
+
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect("192.168.2.5", 1883, 60)
+        client.loop_forever()
