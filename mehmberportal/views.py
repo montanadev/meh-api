5c817cc50dd10c6bfe9541c533b3fdb5246543c0
Jonny Santy <jonny.santy@mt.gov>
2023-05-02 01:01:49 -0600

    achieved basic gpg security

--- mehmberportal/views.py
+++ mehmberportal/views.py
@@ -1,3 +1,6 @@
+import datetime
+
+import gnupg
 import paho.mqtt.client as mqtt
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
@@ -40,17 +43,40 @@ def payment_history(request):
                    'membership_history': membership_history})
 
 
+def calc_payload_to_unlock():
+    gpg = gnupg.GPG()  #  get at  https://gpg4win.org/
+    #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    #sudo apt-get install gnupg
+    # regardless of OS, you'll need to run:    gpg --import ./door_pub_key.asc
+    # gpg --edit-key FB6A37AAAFA7ACD7571AB220EE4EFDD43B4A8504
+    # trust
+    # 5
+    # y
+    # save
+    key_data = SECRETS['door']['pubcert']
+    import_result = gpg.import_keys(key_data)
+    if import_result.count == 0:
+        raise ValueError("Key import failed")
+
+    salty_bastard = "friend"
+    message = SECRETS['door']['psk'] + salty_bastard + str(datetime.datetime.now().microsecond)
+    encrypted_data = gpg.encrypt(message, import_result.fingerprints[0])
+
+    return encrypted_data.data
+
+
 @login_required
 def unlock(request):
     amount_due = mehmbership_due(request.user)
     active_member = is_active_member(request.user)
     membership_in_good_standing = amount_due <= (30 * 3) and active_member
     access_granted = membership_in_good_standing
-    if (access_granted):
+    if access_granted:
         client = mqtt.Client()
         client.connect(SECRETS['mqtt']['host'], SECRETS['mqtt']['port'], 60)
-        # TODO sign this.  Maybe use JWT... probably not because then I'd have to keep track of time securely... NTP FTL
-        client.publish('meh/meh-api/door/access', payload="friend",
+        # TODO include private gpg key on door iot.  Decrypt/validate payload. store/invalidate timestamp as nonce on TF
+        unlock = calc_payload_to_unlock()
+        client.publish('meh/meh-api/door/access', payload=unlock,
                        qos=0, retain=False)
         mehmber_accessed_hackspace(request)
 
