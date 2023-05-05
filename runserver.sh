33452455f4ec911307679375d0c5b6a058317aba
Jonny Santy <jonny.santy@mt.gov>
2023-05-04 21:30:53 -0600

    included migrations in runserver

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd /usr/bin/meh-api/
 export PATH="/home/meh/.local/bin:$PATH"
+poetry run python3 manage.py makemigrations
+poetry run python3 manage.py migrate
 poetry install
 poetry run python3 manage.py runserver 0.0.0.0:80
