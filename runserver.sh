18121dc98f384f7534f5ce8ec336ef01fb1cab7a
Jonny Santy <jonny.santy@mt.gov>
2023-05-07 01:26:36 -0600

    removed bogus -p flag

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /usr/bin/meh-api/meh-api/
 git -C /usr/bin/meh-api/meh-api pull
-/home/meh/.local/bin/poetry install -p /usr/bin/meh-api/meh-api/
+cd /usr/bin/meh-api/meh-api/ && /home/meh/.local/bin/poetry install
 /home/meh/.local/bin/poetry run python3 /usr/bin/meh-api/meh-api/manage.py makemigrations
 /home/meh/.local/bin/poetry run python3 /usr/bin/meh-api/meh-api/manage.py migrate
 /home/meh/.local/bin/poetry run python3 /usr/bin/meh-api/meh-api/manage.py runserver 0.0.0.0:80
